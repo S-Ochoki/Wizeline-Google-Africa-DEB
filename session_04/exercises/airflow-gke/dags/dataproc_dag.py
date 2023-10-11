9ba8422fa013a1b4f259dd111e415539ebd8d726
Sidney Isoe <87696837+S-Ochoki@users.noreply.github.com>
2023-10-12 01:27:06 +0300

    Update dataproc_dag.py machine type to "n2-standard-2"

--- session_04/exercises/airflow-gke/dags/dataproc_dag.py
+++ session_04/exercises/airflow-gke/dags/dataproc_dag.py
@@ -41,12 +41,12 @@ REGION = "me-central1"
 CLUSTER_CONFIG = {
     "master_config": {
         "num_instances": 1,
-        "machine_type_uri": "n1-standard-2",
+        "machine_type_uri": "n2-standard-2",
         "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 32},
     },
     "worker_config": {
         "num_instances": 2,
-        "machine_type_uri": "n1-standard-2",
+        "machine_type_uri": "n2-standard-2",
         "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 32},
     },
     # "secondary_worker_config": {
