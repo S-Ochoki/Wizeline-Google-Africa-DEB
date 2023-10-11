4aac1a6119f7a62a6952d93b30364f624714cc86
Sidney Isoe <87696837+S-Ochoki@users.noreply.github.com>
2023-10-12 00:43:55 +0300

    Update dataproc_dag.py zone change

--- session_04/exercises/airflow-gke/dags/dataproc_dag.py
+++ session_04/exercises/airflow-gke/dags/dataproc_dag.py
@@ -33,6 +33,7 @@ CLUSTER_NAME = "wizeline-deb-dataproc"
 MOVIE_PYSPARK_FILE_URI = "gs://wizeline_bootcamp_bucket/SPARK_JOB/movie_review_positive_sentiment.py"
 LOG_PYSPARK_FILE_URI = "gs://wizeline_bootcamp_bucket/SPARK_JOB/log_review_processing.py"
 REGION = "us-central1"
+ZONE = "us-central1-b"
 
 
 
