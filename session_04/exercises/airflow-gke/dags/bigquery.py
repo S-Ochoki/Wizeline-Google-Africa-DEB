ae94f2d772b2d734083431e23a7e749890d8f5af
Sidney Isoe <87696837+S-Ochoki@users.noreply.github.com>
2023-10-18 17:43:18 +0300

    Update bigquery.py new task groups

--- session_04/exercises/airflow-gke/dags/bigquery.py
+++ session_04/exercises/airflow-gke/dags/bigquery.py
@@ -202,9 +202,10 @@ with DAG(
     # parallel_upload_gcs_data = BitShift(task_id="parallel_upload_gcs_data", tasks=upload_gcs_data_tasks)
         
     gcs_data_uploaded = DummyOperator(task_id="gcs_data_uploaded", trigger_rule=TriggerRule.ALL_SUCCESS)
-    
+
+    create_bq_table_tasks = []
     for table_id, columns in table_schemas.items():
-        create_bq_tables = BigQueryCreateEmptyTableOperator(
+        create_bq_table = BigQueryCreateEmptyTableOperator(
             task_id=f'create_{table_id}_table',
             table_id=f'{table_id}',
             schema_fields=columns,
@@ -213,10 +214,12 @@ with DAG(
             project_id=GCP_PROJECT_ID,      
             location='US',
         )
+        create_bq_table_tasks.append(create_bq_table)
         
     bq_tables_created = DummyOperator(task_id="bq_tables_created", trigger_rule=TriggerRule.ALL_SUCCESS)
 
     # Create a BigQueryInsertJobOperator to execute the insert query
+    insert_dim_data_tasks=[]
     for table_id, insert_query in table_insert_queries.items():
         if table_id != 'fact_movie_analytics':
             insert_dim_data = BigQueryInsertJobOperator(
@@ -231,6 +234,7 @@ with DAG(
                 location='US',  # Set the appropriate location
                 gcp_conn_id=GCP_CONN_ID,
             )
+            insert_dim_data_tasks.append(insert_dim_data)
 
     dim_tables_populated = DummyOperator(task_id="dim_tables_populated", trigger_rule=TriggerRule.ALL_SUCCESS)
     
@@ -250,4 +254,4 @@ with DAG(
 
     end_workflow = DummyOperator(task_id="end_workflow", trigger_rule=TriggerRule.ONE_SUCCESS)
 
-    start_workflow >> create_dataset >> upload_gcs_data_tasks >> gcs_data_uploaded >> [create_bq_tables] >> bq_tables_created >> [insert_dim_data] >> dim_tables_populated >> insert_fact_data >> end_workflow
+    start_workflow >> create_dataset >> upload_gcs_data_tasks >> gcs_data_uploaded >> create_bq_table_tasks >> bq_tables_created >> insert_dim_data_tasks >> dim_tables_populated >> insert_fact_data >> end_workflow
